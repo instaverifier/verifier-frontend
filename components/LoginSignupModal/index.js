5c2e48e17928aa645d17f84d4c272685703bdfca
Rohit Maheshwari <rohit.maheshwari@createbytes.com>
2024-03-30 18:14:41 +0530

    api integration

--- components/LoginSignupModal/index.js
+++ components/LoginSignupModal/index.js
@@ -2,14 +2,24 @@ import { Button, Col, Form, Input, Modal, Row } from "antd";
 import React, { useState } from "react";
 import LoginForm from "./loginForm";
 import SignUpform from "./SignUpForm";
+import axios from "axios";
 
 const LoginSignUpModal = ({ loginModalOpen, setLoginModalOpen }) => {
   const [currentForm, setCurrentForm] = useState("Login");
   const handleCancel = (e) => {
     setLoginModalOpen(false);
   };
-  const onLoginFinish = (values) => {
+  const onLoginFinish = async (values) => {
     console.log("values", values);
+    try {
+      const response = await axios.post(
+        `http://verifer-backend.onrender.com/v1/verifer/user/login`, // Assuming your backend server exposes an endpoint like this
+        values
+      );
+      console.log("response", response);
+    } catch (error) {
+      console.error("Error:", error);
+    }
   };
   const onSignUpFinish = (values) => {
     console.log("vvv", values);
